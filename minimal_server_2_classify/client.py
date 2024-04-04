3ffe16b6352dcf7cd3adae5bcba4e5bf29ac185c
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-04-04 15:34:31 -0300

    Update client.py to accept image path and server URL as command line arguments

--- minimal_server_2_classify/client.py
+++ minimal_server_2_classify/client.py
@@ -1,18 +1,34 @@
 import json
 import requests
 from rich import print
+from sys import argv
 
 
 def run():
-    url = "http://localhost:8000/predict"
+    # argv[1] is the image path
+    # argv[2] is the server url
 
-    image_path = "/mnt/c/Users/adria/Downloads/images.jpeg"
+    print(f"Arguments: {argv}")
+
+    if len(argv) < 3:
+        print("Usage: poetry run client <image_path> <server_url>")
+        return
+    elif len(argv) > 3:
+        print("Too many arguments")
+        return
+
+    image_path = argv[1]
+    url = argv[2]
+
+    # url = "http://localhost:8000/predict"
+
+    # image_path = "/mnt/c/Users/adria/Downloads/images.jpeg"
 
     with open(image_path, "rb") as file:
         image_data = file.read()
 
     files = {"image": ("image.jpg", image_data, "image/jpeg")}
-    response = requests.post(url, files=files)
+    response = requests.post(f"{url}/predict", files=files)
 
     if response.status_code == 200:
         response = json.loads(response.text)
